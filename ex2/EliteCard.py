5f4d4c92f58eacde5f311b02a4f85488b075ecde
Blxee <aminedev57@gmail.com>
2026-01-24 17:43:18 +0100

    Added: progress on ex2

---
+++ ex2/EliteCard.py
@@ -0,0 +1,9 @@
+from ex0.Card import Card
+from ex2.Magical import Magical
+from ex2.Combatable import Combatable
+
+
+class EliteCard(Card, Combatable, Magical):
+    def play(self, game_state: dict) -> dict
+    def attack(self, target) -> dict
+    def cast_spell(self, spell_name: str, targets: list) -> dict
